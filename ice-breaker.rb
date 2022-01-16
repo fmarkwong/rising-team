b085774b6c85c179bd46e647eb2d3759c2977b25
Mark Wong <mark@wonderschool.com>
2022-01-16 02:45:04 -0800

    refactorings

--- ice-breaker.rb
+++ ice-breaker.rb
@@ -9,6 +9,7 @@ end
 
 def ask_many_questions(questions)
   input = ""
+
   loop do
     ask_next_question(questions)
     puts "Press any key to stop"
@@ -20,30 +21,30 @@ def ask_many_questions(questions)
     rescue Timeout::Error
       input = "none"
     end
+
     break if input != "none" 
   end
 end
 
-questions = QuestionData.new
-
-loop do
-  puts
-  puts "What would you like Magic Hat to do?"
+def print_prompt
+  puts "\nWhat would you like Magic Hat to do?"
   puts "a: Ask one question"
   puts "b: Ask many questions"
   puts "c: Quit"
+end
 
-  input = QuestionIO.get_user_input 
+#main loop
+questions = QuestionData.new
 
-  if input == 'a' 
-    ask_next_question(questions)
-  elsif input == 'b'
-    ask_many_questions(questions)
-  elsif input == 'c' || input == "\u0003" # ctrl C
-    exit(1)
+loop do
+  print_prompt
+
+  case QuestionIO.get_user_input
+  when 'a' then ask_next_question(questions)
+  when 'b' then ask_many_questions(questions)
+  when 'c', "\u0003" then exit(1)
   else
-    puts
-    puts "Please enter 'a', 'b', or 'c'" 
+    puts "\nPlease enter 'a', 'b', or 'c'" 
   end
 end
 
